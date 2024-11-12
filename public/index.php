b1c4a9e2ab6cd822879f2f54ab1d1a91ee1bda35
Игорь Комиссаров <gr423_koian@lab-404-9.tomtit-tomsk.ru>
2024-11-12 15:46:07 +0700

    first commit

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <a href="app/registration.php">Регистрация</a>&nbsp&nbsp&nbsp&nbsp
+    <a href="app/login.php">Авторизация</a>
+
+</body>
+</html>
